4f149c8b7aae052d695ab13f64eac4d47e39600b
gabrielsavian <gabrielsvnzrd@gmail.com>
2021-12-20 14:45:41 -0300

    feat: create user func created

--- otp/functions/index.js
+++ otp/functions/index.js
@@ -1,9 +1,16 @@
 const functions = require("firebase-functions");
+const createUser = require("./create_user");
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+exports.helloWorld = functions.https.onRequest((request, response) => {
+  functions.logger.info("Hello logs!", {structuredData: true});
+  response.send("Hello from Firebase!");
+});
+
+exports.goodBye = functions.https.onRequest((request, response) => {
+  response.send('GoodBye');
+});
+
+exports.createUser = functions.https.onRequest(createUser);
