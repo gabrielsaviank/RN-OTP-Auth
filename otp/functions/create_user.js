ce949a7c8d35cd2152398da9dbc79933056af030
gabrielsavian <gabrielsvnzrd@gmail.com>
2021-12-21 20:55:41 -0300

    feat: added functions

--- otp/functions/create_user.js
+++ otp/functions/create_user.js
@@ -11,6 +11,11 @@ module.exports = (req, res) => {
   // format the phone to remove dashes and parenthesis
   const phone = String(req.body.phone).replace(/[^\d]/g);
   // create a new user account using the phone nummer
+  admin.auth().createUser({
+    uuid: phone,
+  }).then((user) => {
+    res.send(user);
+  }).catch((err) => res.status(422).send({error: err}));
 
   // respond to user request
   res.send(req.body);
