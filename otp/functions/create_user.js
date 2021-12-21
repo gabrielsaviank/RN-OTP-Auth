dcc0d02b231b11d3ca7f685e9e03b8c0aae676ee
gabrielsavian <gabrielsvnzrd@gmail.com>
2021-12-21 20:34:17 -0300

    feat: create user

--- otp/functions/create_user.js
+++ otp/functions/create_user.js
@@ -1,3 +1,17 @@
-module.exports = function () {
+const admin = require("firebase-admin");
 
+module.exports = (req, res) => {
+  // Verify the user provided phone
+  if (!req.body.phone) {
+    return res.status(422).send({
+      error: "Bad Input",
+    });
+  }
+
+  // format the phone to remove dashes and parenthesis
+  const phone = String(req.body.phone).replace(/[^\d]/g);
+  // create a new user account using the phone nummer
+
+  // respond to user request
+  res.send(req.body);
 };
