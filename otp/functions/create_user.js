475fc71ca221108e384d76bd6b46b28e252fb0f2
gabrielsavian <gabrielsvnzrd@gmail.com>
2021-12-21 21:04:20 -0300

    feat: comments cleanup

--- otp/functions/create_user.js
+++ otp/functions/create_user.js
@@ -1,21 +1,16 @@
 const admin = require("firebase-admin");
 
 module.exports = (req, res) => {
-  // Verify the user provided phone
   if (!req.body.phone) {
     return res.status(422).send({
       error: "Bad Input",
     });
   }
 
-  // format the phone to remove dashes and parenthesis
   const phone = String(req.body.phone).replace(/[^\d]/g);
-  // create a new user account using the phone nummer
   admin.auth().createUser({
     uid: phone,
   }).then((user) => {
     res.send(user);
   }).catch((err) => res.status(422).send({error: err}));
-
-  // respond to user request
 };
