83ce24a746e3b3daa75520801776da233eaa8b1b
gabrielsavian <gabrielsvnzrd@gmail.com>
2021-12-28 14:29:50 -0300

    feat: sending texts

--- otp/functions/request_one_time_password.js
+++ otp/functions/request_one_time_password.js
@@ -1,4 +1,5 @@
 const admin = require("firebase-admin");
+const twilio = require("./twilio");
 
 module.exports = function(req, res) {
   if (!req.body.phone) {
@@ -10,6 +11,12 @@ module.exports = function(req, res) {
   admin.auth().getUser(phone)
       .then( (userRecord) => {
         const code = Math.floor((Math.random() * 8999 + 1000));
+
+        twilio.messages.create({
+          body: "Your code is " + code,
+          to: phone,
+          from: "+12245215506",
+        });
       }).catch((err) => {
         res.status(422).send({error: err});
       });
