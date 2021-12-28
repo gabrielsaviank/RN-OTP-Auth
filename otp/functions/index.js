53eea823f063963714b1b15a032d750ff45c73b2
gabrielsavian <gabrielsvnzrd@gmail.com>
2021-12-28 15:39:49 -0300

    feat: added verify method

--- otp/functions/index.js
+++ otp/functions/index.js
@@ -1,8 +1,10 @@
 const admin = require("firebase-admin");
 const functions = require("firebase-functions");
+
 const createUser = require("./create_user");
 const serviceAccount = require("./service_account.json");
 const requestOneTimePassword = require("./request_one_time_password");
+const verifyOneTimePassword = require("./verify_one_time_password");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
@@ -14,4 +16,8 @@ exports.createUser = functions.https.onRequest(
 
 exports.requestOneTimePassword = functions.https.onRequest(
     requestOneTimePassword
+);
+
+exports.verifyOneTimePassword = functions.https.onRequest(
+    verifyOneTimePassword
 );
